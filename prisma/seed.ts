2bd513d477cf11a4897e19d0520e242dcadfdd36
vladyslavsoruk <sorukv@gmail.com>
2025-07-15 16:19:58 +0300

    Day deleted from Prisma

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,4 @@
-import { Day, PrismaClient, UserSex } from "@prisma/client";
+import { PrismaClient, UserSex } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
@@ -78,23 +78,23 @@ async function main() {
   }
 
   // LESSON
-  for (let i = 1; i <= 30; i++) {
-    await prisma.lesson.create({
-      data: {
-        name: `Lesson${i}`,
-        day: Day[
-          Object.keys(Day)[
-            Math.floor(Math.random() * Object.keys(Day).length)
-          ] as keyof typeof Day
-        ],
-        startTime: new Date(new Date().setHours(new Date().getHours() + 1)),
-        endTime: new Date(new Date().setHours(new Date().getHours() + 3)),
-        subjectId: (i % 10) + 1,
-        classId: (i % 6) + 1,
-        teacherId: `teacher${(i % 15) + 1}`,
-      },
-    });
-  }
+  // for (let i = 1; i <= 30; i++) {
+  //   await prisma.lesson.create({
+  //     data: {
+  //       name: `Lesson${i}`,
+  //       day: Day[
+  //         Object.keys(Day)[
+  //           Math.floor(Math.random() * Object.keys(Day).length)
+  //         ] as keyof typeof Day
+  //       ],
+  //       startTime: new Date(new Date().setHours(new Date().getHours() + 1)),
+  //       endTime: new Date(new Date().setHours(new Date().getHours() + 3)),
+  //       subjectId: (i % 10) + 1,
+  //       classId: (i % 6) + 1,
+  //       teacherId: `teacher${(i % 15) + 1}`,
+  //     },
+  //   });
+  // }
 
   // PARENT
   for (let i = 1; i <= 25; i++) {
